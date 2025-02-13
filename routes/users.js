532963714a43e8de042fab73885e121a03131335
tenawan2gecko <artem.tchalunin@yandex.ru>
2025-02-13 10:09:30 +0300

    Добавлена директория client без вложенного репозитория

---
+++ routes/users.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const { login, register, current } = require("../controllers/users");
+const {auth} = require('../middleware/auth')
+
+
+/* GET users listing. */
+router.post("/login", login);
+router.post("/register", register);
+router.get("/current", auth, current);
+
+module.exports = router;
